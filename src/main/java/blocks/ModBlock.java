605caa6366e8afe2fce8afa579a581566968b652
CHRISTOPHER-XPS\cmw23 <cwood@qualtrax.com>
2018-05-22 18:09:21 -0400

    Got working explosions for items

--- src/main/java/blocks/ModBlock.java
+++ src/main/java/blocks/ModBlock.java
@@ -1,12 +1,22 @@
 package blocks;
 
+import javax.annotation.Nullable;
+
 import com.example.examplemod.ExampleMod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
 
 public class ModBlock extends Block {
 
@@ -20,6 +30,24 @@ public class ModBlock extends Block {
 		setUnlocalizedName(name);
 		setRegistryName(name);
 	}
+	
+	@Override
+    public boolean onBlockActivated(World worldIn,
+            BlockPos pos,
+            IBlockState state,
+            EntityPlayer playerIn,
+            EnumHand hand,
+            EnumFacing side,
+            float hitX,
+            float hitY,
+            float hitZ)
+    {
+    	playerIn.jump();
+    	playerIn.performHurtAnimation();
+    	Explosion explosion = worldIn.createExplosion(playerIn, playerIn.posX, playerIn.posY, playerIn.posZ, 100.0f, true);
+    	explosion.doExplosionA();
+		return true;
+    }
 	
 	public void registerItemModel(Item itemBlock) {
 		ExampleMod.proxy.registerItemRenderer(itemBlock, 0, name);
--- src/main/java/blocks/UdderBlock.java
+++ src/main/java/blocks/UdderBlock.java
@@ -1,7 +1,17 @@
 package blocks;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.sound.SoundEvent;
 
 public class UdderBlock extends ModBlock
 {
@@ -10,5 +20,21 @@ public class UdderBlock extends ModBlock
         super(Material.CAKE, "udder_block");
         
 		setCreativeTab(CreativeTabs.MATERIALS);
+    }
+    
+    public boolean onBlockActivated(World worldIn,
+            BlockPos pos,
+            IBlockState state,
+            EntityPlayer playerIn,
+            EnumHand hand,
+            @Nullable ItemStack heldItem,
+            EnumFacing side,
+            float hitX,
+            float hitY,
+            float hitZ)
+    {
+    	playerIn.jump();
+    	playerIn.performHurtAnimation();
+		return true;
     }
 }
